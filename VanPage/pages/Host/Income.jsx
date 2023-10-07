c3456b11e4be2ae56ae4bc2b54f779f00e3113e6
Erika Misheva <96700454+erika-misheva@users.noreply.github.com>
2023-10-07 18:37:38 +0200

    Delete VanPage directory

--- VanPage/pages/Host/Income.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Income() {
-    return (
-        <div>
-            <h1>Host Income here</h1>
-        </div>
-    );
-}
--- VanPage/pages/Host/Reviews.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Reviews() {
-    return (
-        <div>
-            <h1>Host Reviews here</h1>
-        </div>
-    );
-}
